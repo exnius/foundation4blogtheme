10919bbbb5ce1cbc56f242db262d4462853a6b38
gatespace <gatespace.k@gmail.com>
2013-05-21 15:30:48 +0900

    add <li class="divider"></li> in foundation4blogtheme_walker_nav_menu class

--- inc/extras.php
+++ inc/extras.php
@@ -82,6 +82,37 @@ class foundation4blogtheme_walker_nav_menu extends Walker_Nav_Menu {
 		$element->classes[] = ($element->current || $element->current_item_ancestor || in_array("current-page-ancestor", $element->classes)) ? 'active' : '';
 		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
 	}
+
+	// add <li class="divider"></li>
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+	
+		$class_names = $value = '';
+	
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+	
+		$class_names = join( ' ', apply_filters( 'foundation4blogtheme_walker_nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+	
+		$id = apply_filters( 'foundation4blogtheme_walker_nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+	
+		$output .= $indent . '<li class="divider"></li><li' . $id . $value . $class_names .'>';
+	
+		$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+		$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+		$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+		$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+	
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+	
+		$output .= apply_filters( 'foundation4blogtheme_walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
 } // end foundation4blogtheme_walker_nav_menu class
 
 /**
