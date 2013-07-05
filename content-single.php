4cee6623bf6bc63e9083bc129f881d4e0d16f49c
gatespace <gatespace.k@gmail.com>
2013-07-05 18:03:48 +0900

    support: post thumbnail
    support: post thumbnail #2

--- content-single.php
+++ content-single.php
@@ -6,27 +6,33 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
 
-		<div id="postedon" class="entry-meta">
-			<?php foundation4blogtheme_posted_on(); ?>
-			<?php if ( comments_open() ) : ?>
-				<i class="genericon genericon-comment"></i>
-				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'foundation4blogtheme' ) . '</span>', __( '1 Comment', 'foundation4blogtheme' ), __( '% Comments', 'foundation4blogtheme' ) ); ?>
-			<?php endif; // comments_open() ?>
-			<?php edit_post_link( __( 'Edit', 'foundation4blogtheme' ), ' <span class="edit-link"><i class="genericon genericon-edit"></i>', '</span>' ); ?>
-		</div><!-- #postedon.entry-meta -->
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="entry-post_thumbnail"><?php the_post_thumbnail('thumbnail'); ?></div>
+	<?php endif; ?>
 
-		<div id="postedcategory" class="entry-meta"><i class="genericon genericon-category"></i><?php the_category(', '); ?></div><!-- #postedcategory.entry-meta -->
-		<?php
-			$tag_list = get_the_tag_list( '', __( ', ', 'foundation4blogtheme' ) );
-			if ( '' != $tag_list ) {
-		?>
-		<div id="postedtag" class="entry-meta"><?php echo ' <i class="genericon genericon-tag"></i>' . $tag_list; ?></div><!-- #postedtag.entry-meta -->
-		<?php
-			}
-		?>
-
+		<div class="entry-header-text">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+	
+			<div id="postedon" class="entry-meta">
+				<?php foundation4blogtheme_posted_on(); ?>
+				<?php if ( comments_open() ) : ?>
+					<i class="genericon genericon-comment"></i>
+					<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'foundation4blogtheme' ) . '</span>', __( '1 Comment', 'foundation4blogtheme' ), __( '% Comments', 'foundation4blogtheme' ) ); ?>
+				<?php endif; // comments_open() ?>
+				<?php edit_post_link( __( 'Edit', 'foundation4blogtheme' ), ' <span class="edit-link"><i class="genericon genericon-edit"></i>', '</span>' ); ?>
+			</div><!-- #postedon.entry-meta -->
+	
+			<div id="postedcategory" class="entry-meta"><i class="genericon genericon-category"></i><?php the_category(', '); ?></div><!-- #postedcategory.entry-meta -->
+			<?php
+				$tag_list = get_the_tag_list( '', __( ', ', 'foundation4blogtheme' ) );
+				if ( '' != $tag_list ) {
+			?>
+			<div id="postedtag" class="entry-meta"><?php echo ' <i class="genericon genericon-tag"></i>' . $tag_list; ?></div><!-- #postedtag.entry-meta -->
+			<?php
+				}
+			?>
+		</div>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
--- content.php
+++ content.php
@@ -6,37 +6,43 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="entry-post_thumbnail"><?php the_post_thumbnail('thumbnail'); ?></div>
+	<?php endif; ?>
+
+		<div class="entry-header-text">
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 		
 		<?php if ( 'post' == get_post_type() ) : ?>
 
-		<div id="postedon" class="entry-meta">
-			<?php foundation4blogtheme_posted_on(); ?>
-			<?php if ( comments_open() ) : ?>
-				<i class="genericon genericon-comment"></i>
-				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'foundation4blogtheme' ) . '</span>', __( '1 Comment', 'foundation4blogtheme' ), __( '% Comments', 'foundation4blogtheme' ) ); ?>
-			<?php endif; // comments_open() ?>
-			<?php edit_post_link( __( 'Edit', 'foundation4blogtheme' ), ' <span class="edit-link"><i class="genericon genericon-edit"></i>', '</span>' ); ?>
-		</div><!-- #postedon.entry-meta -->
+			<div id="postedon" class="entry-meta">
+				<?php foundation4blogtheme_posted_on(); ?>
+				<?php if ( comments_open() ) : ?>
+					<i class="genericon genericon-comment"></i>
+					<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'foundation4blogtheme' ) . '</span>', __( '1 Comment', 'foundation4blogtheme' ), __( '% Comments', 'foundation4blogtheme' ) ); ?>
+				<?php endif; // comments_open() ?>
+				<?php edit_post_link( __( 'Edit', 'foundation4blogtheme' ), ' <span class="edit-link"><i class="genericon genericon-edit"></i>', '</span>' ); ?>
+			</div><!-- #postedon.entry-meta -->
 
-		<div id="postedcategory" class="entry-meta"><i class="genericon genericon-category"></i><?php the_category(', '); ?></div><!-- #postedcategory.entry-meta -->
-		<?php
-			$tag_list = get_the_tag_list( '', __( ', ', 'foundation4blogtheme' ) );
-			if ( '' != $tag_list ) {
-		?>
-		<div id="postedtag" class="entry-meta"><?php echo ' <i class="genericon genericon-tag"></i>' . $tag_list; ?></div><!-- #postedtag.entry-meta -->
-		<?php
-			}
-		?>
-
-		<?php else : ?>
-
-		<div class="entry-meta">
-			<?php edit_post_link( __( 'Edit', 'foundation4blogtheme' ), '<span class="edit-link"><i class="genericon genericon-edit"></i>', '</span>' ); ?>
-		</div><!-- .entry-meta -->
+			<div id="postedcategory" class="entry-meta"><i class="genericon genericon-category"></i><?php the_category(', '); ?></div><!-- #postedcategory.entry-meta -->
+			<?php
+				$tag_list = get_the_tag_list( '', __( ', ', 'foundation4blogtheme' ) );
+				if ( '' != $tag_list ) {
+			?>
+			<div id="postedtag" class="entry-meta"><?php echo ' <i class="genericon genericon-tag"></i>' . $tag_list; ?></div><!-- #postedtag.entry-meta -->
+			<?php
+				}
+			?>
+	
+			<?php else : ?>
+	
+			<div class="entry-meta">
+				<?php edit_post_link( __( 'Edit', 'foundation4blogtheme' ), '<span class="edit-link"><i class="genericon genericon-edit"></i>', '</span>' ); ?>
+			</div><!-- .entry-meta -->
 
 		<?php endif; ?>
-
+		</div>
 	</header><!-- .entry-header -->
 
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
